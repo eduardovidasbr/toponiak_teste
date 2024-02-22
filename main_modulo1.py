64e90389cb8cd7a8411bdaf615ad057fbf8bb663
eduardo <102316412+eduardovidasbr@users.noreply.github.com>
2024-02-22 01:06:08 -0400

    Delete main_modulo1.py

--- main_modulo1.py
+++
@@ -1,6 +0,0 @@
-contador = 0
-quantidade = int(input("Quantas Réguas você vai levar: "))
-while contador < 5:
-    contador += 1
-    print("Quantas Tomadas tem na Régua: {}: {}".format(contador, quantidade))
-print('Muito Bem, você conseguira, colocar 21 Tomadas, Cuidado Queimar Tudo :)')
